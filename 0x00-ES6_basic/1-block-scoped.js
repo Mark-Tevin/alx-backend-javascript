b34a3d3f88673739a488aea8ecb5a3243eafa20a
Mark-Tevin <tevinsmark@gmail.com>
2024-10-02 03:37:35 +0000

    debugging Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;  // Reassigning outer-scoped variable
-    task2 = false; // Reassigning outer-scoped variable
+    const task = true;  // Reassigning outer-scoped variable
+    const task2 = false; // Reassigning outer-scoped variable
   }
 
   return [task, task2];
