9b8e2e9a0bc60a00b6a511011877deb8a1e5a243
Mark-Tevin <tevinsmark@gmail.com>
2024-10-03 09:58:35 +0000

    debug Arrow functions

--- 0x00-ES6_basic/2-main.js
+++ 0x00-ES6_basic/2-main.js
@@ -1,5 +1,6 @@
 import getNeighborhoodsList from './2-arrow.js';
 
+const self = this;
 const neighborhoodsList = new getNeighborhoodsList();
 const res = neighborhoodsList.addNeighborhood('Noe Valley');
 console.log(res);
