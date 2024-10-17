f5e2c26ab0280b4d8591e16fb77f13b5d6b9edaf
Mark-Tevin <tevinsmark@gmail.com>
2024-10-17 21:10:58 +0000

    Let's make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,9 @@
 import ClassRoom from './0-classroom.js';
 
 export default function initializeRooms() {
-  const roomSizes = [19, 20, 34];
-  const rooms = [];
-
-  for (const size of roomSizes) {
-    const room = new ClassRoom(size);
-    rooms.push(room);
-  }
-
-  return rooms;
+  const class1 = new ClassRoom(19);
+  const class2 = new ClassRoom(20);
+  const class3 = new ClassRoom(34);
+  const newList = [class1, class2, class3];
+  return (newList);
 }
